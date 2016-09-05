f5bf49e68d4e6298496d4eceb3878f893bca7a13
bingbo <ibingbo.zh@gmail.com>
2016-09-05 10:03:33 +0800

    up

--- bing_person.h
+++ bing_person.h
@@ -1,6 +1,7 @@
 #ifndef BING_PERSON_H
 #define BING_PERSON_H
 
+//声明全局变量的引用
 extern zend_class_entry *person_ce;
 
 PHP_MINIT_FUNCTION(bing_person);
--- bing_service.h
+++ bing_service.h
@@ -5,6 +5,7 @@
 #define BING_SERVICE_PROPERTY_NAME_RESULT "_result"
 
 
+//声明全局变量的引用
 extern zend_class_entry *bing_service_ce,*bing_service_parent_ce,*bing_service_interface_ce;
 
 PHP_MINIT_FUNCTION(bing_service);
